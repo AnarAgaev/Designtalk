bee4afc1671cf563e3df7b6789d92b196fc78e37
Anar Agaev <anar.n.agaev@gmail.com>
2020-02-28 21:47:34 +0300

    add reactions to headere

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,67 @@
 import React, {Component} from "react";
-import {Container} from "react-bootstrap";
-
-import './header.scss';
-
+import { Container } from "react-bootstrap";
 import Logotype from "../logotype";
 import Nav from "../nav";
 import SocialList from "../socials-list";
+import './header.scss';
 
 export default class Header extends Component {
+  state = {
+    background: false,
+    visible: true,
+    scrolled: 0
+  };
 
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
   }
-
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   }
 
+  /*
+  * handleScroll implements functionality, the header
+  * changes its background and then disappears
+  * when the user scrolls the screen window.
+  */
   handleScroll = () => {
-    //console.log(window.scrollY);
+    const { scrolled } = this.state;
+    let scroll = window.pageYOffset;
+
+    (scroll > 30)
+      ? this.setState({ background: true })
+      : this.setState({ background: false });
+
+    if (scroll > 700) {
+      if (scroll < scrolled) {
+        this.setState({
+          visible: true,
+          scrolled: scroll
+        });
+      }
+
+      if (scroll > scrolled + 100) {
+        this.setState({
+          visible: false,
+          scrolled: scroll
+        });
+      }
+    }
   };
 
   render() {
+    const { background, visible } = this.state;
+    let classNames = 'header';
+
+    if (background) {
+      classNames += ' background-show';
+    }
+    if (!visible) {
+      classNames += ' hide';
+    }
+
     return (
-      <header className="header">
+      <header className={ classNames }>
         <Container>
           <Logotype/>
           <Nav/>
