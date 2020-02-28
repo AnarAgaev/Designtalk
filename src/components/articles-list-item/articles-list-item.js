a727614d83f68012f0730ca6b9b8841a45251ef4
Anar Agaev <anar.n.agaev@gmail.com>
2020-02-28 14:37:03 +0300

    rename hashtag to rubric

--- src/components/articles-list-item/articles-list-item.js
+++ src/components/articles-list-item/articles-list-item.js
@@ -1,14 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import './articles-list-item.scss';
-import {Link} from "react-router-dom";
 
-const ArticlesListItem = ({ img, hashtag, caption }) => {
+const ArticlesListItem = ({ img, rubric, caption }) => {
   return (
     <div className="article-list__item">
       <Link to="/" className="article-list__picture"><img src={ img } alt="Image"/></Link>
       <Link to="/" className="article-list__caption"><span>{ caption }</span></Link>
-      <Link to="/" className="article-list__link">{ hashtag }</Link>
+      <Link to="/" className="article-list__link">{ rubric }</Link>
     </div>
   );
 };
--- src/components/pages/main/main.js
+++ src/components/pages/main/main.js
@@ -15,10 +15,10 @@ import img3 from './img/elegantnaya-skandinavskaya-kvartira-v-priglushennyh-tona
 import img4 from './img/malenkaya-kvartira-v-temno-seryh-tonah-v-shvecii-40kvm-pufikhomes-2-1.jpg';
 
 const articlesListData = [
-  { columns: 7, columnClass: '', img: img1, hashtag: '#Маленькие квартиры', caption: 'Светлая квартира с персиковыми стенами на окраине Стокгольма'},
-  { columns: 5, columnClass: '', img: img2, hashtag: '#Маленькие квартиры', caption: 'Интересные цветовые сочетания в интерьере квартиры в Нью-Йорке'},
-  { columns: 6, columnClass: 'pr-5', img: img3, hashtag: '#Маленькие квартиры', caption: 'Элегантная скандинавская квартира в приглушенных тонах'},
-  { columns: 6, columnClass: 'pl-5', img: img4, hashtag: '#Маленькие квартиры', caption: 'Маленькая серая квартира с видом на залив в Стокгольме'}
+  { id: 1, columns: 7, columnClass: '', img: img1, rubric: '#Маленькие квартиры', caption: 'Светлая квартира с персиковыми стенами на окраине Стокгольма'},
+  { id: 2, columns: 5, columnClass: '', img: img2, rubric: '#Маленькие квартиры', caption: 'Интересные цветовые сочетания в интерьере квартиры в Нью-Йорке'},
+  { id: 3, columns: 6, columnClass: 'pr-5', img: img3, rubric: '#Маленькие квартиры', caption: 'Элегантная скандинавская квартира в приглушенных тонах'},
+  { id: 4, columns: 6, columnClass: 'pl-5', img: img4, rubric: '#Маленькие квартиры', caption: 'Маленькая серая квартира с видом на залив в Стокгольме'}
 ];
 
 const Main = () => {
