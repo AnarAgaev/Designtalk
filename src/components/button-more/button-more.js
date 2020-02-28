2d08c4b48c461930a229a145424c7d08845ae4f1
Anar Agaev <anar.n.agaev@gmail.com>
2020-02-28 20:38:01 +0300

    reafacor button more articles

--- src/components/button-more/button-more.js
+++ src/components/button-more/button-more.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, {Component} from "react";
+import { Container } from "react-bootstrap";
 
 import './button-more.scss';
-import {Container} from "react-bootstrap";
 
-const ButtonMore = () => {
-  return (
-    <Container>
-      <div className="d-flex mb-5 justify-content-center">
-        <button className="button px-5 py-4">Показать больше интересного</button>
-      </div>
-    </Container>
-  );
+export default class ButtonMore extends Component {
+  render() {
+    return (
+      <Container>
+        <div className="d-flex mb-5 justify-content-center">
+          <button className="button px-5 py-4" onClick={ () => { console.log('Hello App!') } }>Показать больше интересного</button>
+        </div>
+      </Container>
+    );
+  }
 };
-
-export default ButtonMore;
