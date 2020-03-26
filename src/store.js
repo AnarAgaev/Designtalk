174600ff3fc335cb504f32b787c25d7095c0701f
Anar Agaev <anar.n.agaev@gmail.com>
2020-03-26 11:44:04 +0300

    created own store as used store enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,36 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import reducer from "./reducers/index";
 
-const store = createStore(reducer);
+const stringEnhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+
+  store.dispatch = (action) => {
+    if (typeof action === 'string') {
+      return originalDispatch({
+        type: action
+      });
+    }
+    originalDispatch(action);
+  };
+
+  return store;
+};
+
+const logEnhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+
+  store.dispatch = (action) => {
+    console.log(action);
+    return originalDispatch(action);
+  };
+
+  return store;
+};
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+store.dispatch('HELLO WORLD!');
 
 export default store;
