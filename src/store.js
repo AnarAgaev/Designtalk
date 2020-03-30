2cd5adeeef31115db5cd526df3ed04e03974baa6
Anar Agaev <anar.n.agaev@gmail.com>
2020-03-30 18:37:12 +0300

    added blcker for popular articles slider

--- src/store.js
+++ src/store.js
@@ -1,6 +1,27 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers/index";
 
-const store = createStore(reducer);
+let isBlocked;
+
+const blockMountingMiddleware = () => (dispatch) => (action) => {
+
+  if (action.type === 'TOGGLE_POPULAR_SLIDE' && isBlocked) {
+      return;
+  }
+
+  isBlocked = true;
+  setTimeout(() => isBlocked = false, 300);
+  return dispatch(action);
+};
+
+// const logMiddleware = ({ getState }) => (dispatch) => (action) => {
+//   console.log(action, action.type, getState());
+//   return dispatch(action);
+// };
+
+const store = createStore(reducer, applyMiddleware(
+  // logMiddleware,
+  blockMountingMiddleware
+));
 
 export default store;
