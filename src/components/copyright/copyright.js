190c49ffd886773a9acd1237974304c4edb540d5
Anar Agaev <anar.n.agaev@gmail.com>
2020-04-05 14:08:09 +0300

    added current date to the footer

--- src/components/copyright/copyright.js
+++ src/components/copyright/copyright.js
@@ -2,22 +2,23 @@ import React from "react";
 
 import './copyright.scss';
 
+const date = new Date();
+
 const Copyright = () => {
   return (
     <small className="copy">
       <p>
-        © Общество с ограниченной ответственностью
-        "Цифровое Медиа "Спэйс Билдер" 2020 - 2020.
+        © "Цифровое Медиа "Дизайнталк" 2020 - {`${date.getFullYear()}`}.
       </p>
       <p>
-        Все права защищены. Использование материалов сайта spacebuilder.ru
+        Все права защищены. Использование материалов сайта designtalk.ru
         разрешается только с письменного согласия редакции и при наличии
-        гиперссылки на spacebuilder.ru.
+        гиперссылки на designtalk.ru
       </p>
       <p>
         По вопросам соблюдения интеллектуальных прав обращайтесь по адресу&nbsp;
-        <a href="mailto:intellectualrights@spacebuilder.ru">
-          intellectualrights@spacebuilder.ru
+        <a href="mailto:intellectualrights@designtalk.ru">
+          intellectualrights@designtalk.ru
         </a>
       </p>
     </small>
