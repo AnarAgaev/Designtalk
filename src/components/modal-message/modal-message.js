8ff4a18a9008076472f084c8bdcae91ebb139785
Anar Agaev <anar.n.agaev@gmail.com>
2020-04-13 11:57:10 +0300

    added focus to the button on the modal message component

--- src/components/modal-message/modal-message.js
+++ src/components/modal-message/modal-message.js
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 
 import './modal-message.scss';
 
-const ModalMessage = ({ message, visible, modalMsgHide }) => {
-  const modalClass = visible ?
-    'modal visible' :
-    'modal';
+class ModalMessage extends Component {
+  btnClose = React.createRef();
 
-  return (
-    <div className={ modalClass }>
-      <div className="modal__dialog">
-        <div className="modal__content">
-          <div className="modal__body">
-            <p>{ message }</p>
-          </div>
-          <div className="modal__footer">
-            <button
-              type="button"
-              className="button"
-              onClick={ modalMsgHide }>
-              Закрыть
-            </button>
+  componentDidUpdate() {
+    const node = this.btnClose.current;
+
+    this.props.visible
+      ? node.focus()
+      : node.blur();
+  }
+
+  render() {
+    const {
+      message,
+      visible,
+      modalMsgHide
+    } = this.props;
+
+    const modalClass = visible
+      ? 'modal visible'
+      : 'modal';
+
+    return (
+      <div className={modalClass}>
+        <div className="modal__dialog">
+          <div className="modal__content">
+            <div className="modal__body">
+              <p>{message}</p>
+            </div>
+            <div className="modal__footer">
+              <button
+                ref={ this.btnClose }
+                type="button"
+                className="button"
+                onClick={modalMsgHide}>
+                Закрыть
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default ModalMessage;
