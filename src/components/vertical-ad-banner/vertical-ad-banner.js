e81d3ba253639878d2a5328a6fb4b35aea669570
Anar Agaev <anar.n.agaev@gmail.com>
2020-05-04 12:36:15 +0300

    changed vertical banner

--- src/components/vertical-ad-banner/vertical-ad-banner.js
+++ src/components/vertical-ad-banner/vertical-ad-banner.js
@@ -4,8 +4,8 @@ import "./vertical-ad-banner.scss";
 const VerticalAdBanner = () => {
   return (
     <a target="_blank" rel="noopener noreferrer" className="vertical-ad-banner"
-       href="https://www.lg.com/ru/lg-signature/performance/perfection-realized-refrigerator">
-      <img src="https://designtalk.ru/images/adv/2714279522559089340.jpg" alt="" />
+       href="https://www.glamour.ru/shopping_week/card/">
+      <img src="https://designtalk.ru/images/adv/3585987843283241866.jpg" alt="" />
     </a>
   );
 };
