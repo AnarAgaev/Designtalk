56f674be4ddc7e0346a7cbe47bdcf6796eec759c
Anar Agaev <anar.n.agaev@gmail.com>
2020-05-23 20:29:12 +0300

    rewrited telegram link

--- src/components/sharing-article/social-list-data.js
+++ src/components/sharing-article/social-list-data.js
@@ -12,7 +12,7 @@ const socialItems = [
   { id: 'faInstagram',       icon:  faInstagram,      link: 'https://www.instagram.com/designtalkru' },
   { id: 'faOdnoklassniki',   icon: faOdnoklassniki,   link: 'https://ok.ru/designtalk' },
   { id: 'faFacebookF',       icon: faFacebookF,       link: 'https://www.facebook.com/designtalkru' },
-  { id: 'faTelegramPlane',   icon: faTelegramPlane,   link: 'https://t.me/designtalkru' },
+  { id: 'faTelegramPlane',   icon: faTelegramPlane,   link: 'https://zzap.run/designtalkru' },
   { id: 'faTwitter',         icon: faTwitter,         link: 'https://twitter.com/DesigntalkRu' },
   { id: 'faPinterestP',      icon: faPinterestP,      link: 'https://www.pinterest.ru/designtalkru' }
 ];
